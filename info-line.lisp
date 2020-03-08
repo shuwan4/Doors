04f3779270019b9084ab2f2c053a94425832a3e7
watkinsr <ryanwatkins54@gmail.com>
2020-03-08 17:16:11 +0000

    remove battery info

--- info-line.lisp
+++ info-line.lisp
@@ -17,113 +17,6 @@
 
 (in-package :doors)
 
-(defparameter *batteries* nil)
-
-(defclass battery ()
-  ((name :initarg :name :reader battery-name :initform "---")
-   (path :initarg :path :reader battery-path)))
-
-(defclass battery-set (battery)
-  ((set :initarg :set :accessor battery-set :initform nil))
-  (:default-initargs :name "ALL"))
-
-(define-presentation-type battery () :options ((what-present :name)) :inherit-from 'standard-object)
-
-(define-presentation-method present (object (type battery) stream view &key)
-  (case what-present
-    (:name (format stream "~a" (battery-name object)))
-    (:long (format stream "~a: ~a, ~d%"
-                   (battery-name object)
-                   (battery-status object)
-                   (round (battery-% object)))
-           (unless (string= "UNKNOWN" (battery-status object))
-             (format stream " ~{~2,'0d~^:~} remaining" (battery-remaining-time object))))
-    (t (let* ((percent (battery-% object))
-         (ink (cond
-                ((< percent 20) +red+)
-                ((< 20 percent 50) +orange+)
-                (t +foreground-ink+))))
-         (write-string "[" stream)
-         (with-drawing-options (stream :ink ink)
-           (format stream "~d%~:[-~;+~]" (round percent) (battery-charging-p object)))
-         (write-string "] " stream)))))
-
-(defun read-sysfs (bat file)
-  (read-from-string
-   (string-trim '(#\Newline)
-                (alexandria:read-file-into-string (merge-pathnames file (battery-path bat))))))
-
-(defun init-batteries ()
-  (setf *batteries*
-        (make-instance 'battery-set
-                       :set
-                       (loop for path in (directory "/sys/class/power_supply/*")
-                          when (string= "Battery" (string-trim '(#\Newline) (alexandria:read-file-into-string (merge-pathnames "type" path))))
-                          collect (make-instance 'battery :path path
-                                                 :name (alexandria:lastcar (pathname-directory path)))))))
-
-(init-batteries)
-
-(defmethod battery-energy-full ((bat battery))
-  (read-sysfs bat "energy_full"))
-
-(defmethod battery-energy-full ((bat battery-set))
-  (reduce #'+ (battery-set bat) :key #'battery-energy-full))
-
-(defmethod battery-energy-now ((bat battery))
-  (read-sysfs bat "energy_now"))
-
-(defmethod battery-energy-now ((bat battery-set))
-  (reduce #'+ (battery-set bat) :key #'battery-energy-now))
-
-(defmethod battery-power-now ((bat battery))
-  (read-sysfs bat "power_now"))
-
-(defmethod battery-power-now ((bat battery-set))
-  (reduce #'+ (battery-set bat) :key #'battery-power-now))
-
-(defmethod battery-% (bat)
-  (* 100 (/ (battery-energy-now bat) (battery-energy-full bat))))
-
-(defmethod battery-charging-p ((bat battery))
-  (string= "CHARGING" (symbol-name (read-sysfs bat "status"))))
-
-(defmethod battery-charging-p ((bat battery-set))
-  (loop for x in (battery-set bat) thereis (battery-charging-p x)))
-
-(defmethod battery-status ((bat battery))
-  (symbol-name (read-sysfs bat "status")))
-
-(defmethod battery-status ((bat battery-set))
-  (if (battery-charging-p bat)
-      "CHARGING"
-      "DISCHARGING"))
-
-(defmethod battery-remaining-time ((bat battery))
-  (when (not (zerop (battery-power-now bat)))
-    (let ((time (if (battery-charging-p bat)
-                    (/ (- (battery-energy-full bat) (battery-energy-now bat)) (battery-power-now bat))
-                    (/ (battery-energy-now bat) (battery-power-now bat)))))
-      (multiple-value-bind (h rh) (floor time)
-        (multiple-value-bind (min rmin) (floor (* 60 rh))
-          (list h min (round (* 60 rmin))))))))
-
-(define-presentation-action battery-menu
-    (battery nil doors
-             :documentation "View batteries"
-             :pointer-documentation ((object stream) (present object '((battery) :what-present :long) :stream stream))
-             :menu nil
-             :gesture :select
-             :tester ((object)
-                      (typep object 'battery-set)))
-    (object)
-  (menu-choose (map 'list (lambda (x)
-                            (list (present-to-string x '((battery) :what-present :long)) :value t))
-                    (battery-set object))
-               :scroll-bars nil
-               :y-spacing 10
-               :label "Batteries"))
-
 ;;;; Wireless
 (defun wireless ()
   (let* ((iwoutput (uiop:run-program "iwconfig" :output :string))
@@ -171,7 +64,6 @@
   (format pane " " )
   (multiple-value-bind (sec min h d m y) (decode-universal-time (get-universal-time))
     (format pane "~d/~2,'0d/~2,'0d ~2,'0d:~2,'0d:~2,'0d " y m d h min sec))
-  (present *batteries* '((battery) :what-present :short))
   (format pane "~a|" (wireless))
   (format pane "~a|" (ram-usage))
   (format pane "~a|" (vol))
